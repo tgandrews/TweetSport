a98d5b419f7deaa5a974e910d414718f8a81a239
Tom Andrews <git@tomandrews.co.uk>
2010-06-06 23:50:32 +0100

    Updated from using the word instance to incident. How did I forget that word?

--- incident.rb
+++ incident.rb
@@ -1,9 +1,9 @@
-class MatchInstance
+class MatchIncident
   attr_accessor :type, :minutes, :person, :country
 
   def initialize(value)
     # Set defaults
-    @type = InstanceType::UNKNOWN
+    @type = IncidentType::UNKNOWN
     @minutes = 0
     @person = 'Andrews'
     @country = 'England'
@@ -23,13 +23,13 @@ class MatchInstance
     @country = 'England'
   end
 
-  # When did the instance happen?
+  # When did the Incident happen?
   def determineMinutes
-    if @type == InstanceType::HALF_TIME
+    if @type == IncidentType::HALF_TIME
       @minutes = 45
-    elsif @type == InstanceType::FULL_TIME
+    elsif @type == IncidentType::FULL_TIME
       @minutes = 90
-    elsif @type == InstanceType::GOAL
+    elsif @type == IncidentType::GOAL
       mins = @value.search("strong[@class='minutesIntoMatch']")
       if mins.any?
         @minutes = mins[0].inner_html.strip
@@ -38,30 +38,30 @@ class MatchInstance
   end
 
 
-  # Who was involved in the instance?
+  # Who was involved in the incident?
   def determinePerson
     @person = 'Andrews'
   end
 
-  # What was the type of instance
+  # What was the type of incident
   def determineType
     html = @value.inner_html
     id = @value.attributes['id']
 
     if html.include? 'GOAL: '
-      @type = InstanceType::GOAL
+      @type = IncidentType::GOAL
     elsif id.include? '_HalfTime'
-      @type = InstanceType::HALF_TIME
+      @type = IncidentType::HALF_TIME
     elsif id.include? '_FullTime'
-      @type = InstanceType::FULL_TIME
+      @type = IncidentType::FULL_TIME
     elsif html.include? 'SENT OFF:'
-      @type = InstanceType::SENT_OFF
-
+      @type = IncidentType::SENT_OFF
+    end
   end
 
 end
 
-class InstanceType
+class IncidentType
   GOAL      = 1
   SENT_OFF  = 2
   HALF_TIME = 3
--- tweet-sport.rb
+++ tweet-sport.rb
@@ -1,20 +1,18 @@
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
-
-require 'instance'
+require 'incident'
 
 # Get page from BBC site
 doc = open('http://news.bbc.co.uk/sport1/hi/football/live_videprinter/default.stm') { |f| Hpricot(f) }
 
 # Get all the instances
-instances = doc.search("//div[@id='footballVideprinter']/ul/li")
+incidents = doc.search("//div[@id='footballVideprinter']/ul/li")
 
-instances.each do |i|
-  #if instance.
-  inst = MatchInstance.new(i)
-  puts 'Type: ' + inst.type.to_s
-  puts 'Minutes: ' + inst.minutes.to_s
+incidents.each do |i|
+  inc = MatchIncident.new(i)
+  puts 'Type: ' + inc.type.to_s
+  puts 'Minutes: ' + inc.minutes.to_s
   puts i
 end
 
