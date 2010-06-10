6b8c486686746f31133b6a93bcae7ce1c9def5c9
Tom Andrews <git@tomandrews.co.uk>
2010-06-10 20:31:32 +0100

    Cleaned up tabbing

--- tweet-sport.rb
+++ tweet-sport.rb
@@ -10,12 +10,12 @@ doc = open('http://news.bbc.co.uk/sport1/hi/football/live_videprinter/default.st
 incidents = doc.search("//div[@id='footballVideprinter']/ul/li")
 
 incidents.each do |i|
-  inc = MatchIncident.new(i)
-  puts 'Type: ' + inc.type.to_s
-  puts 'Minutes: ' + inc.minutes.to_s
-  puts 'MatchID: ' + inc.matchid.to_s
-  puts 'Person: ' + inc.person.to_s
-  puts 'Country: ' + inc.country.to_s
-  puts i
+    inc = MatchIncident.new(i)
+    puts 'Type: ' + inc.type.to_s
+    puts 'Minutes: ' + inc.minutes.to_s
+    puts 'MatchID: ' + inc.matchid.to_s
+    puts 'Person: ' + inc.person.to_s
+    puts 'Country: ' + inc.country.to_s
+    puts i
 end
 
