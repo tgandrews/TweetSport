0e171a2025c23d51f1682d3d3ab6d24c298ff9fe
Tom Andrews <git@tomandrews.co.uk>
2010-06-10 19:59:47 +0100

    Fixed spacing issues

--- incident.rb
+++ incident.rb
@@ -2,92 +2,92 @@
 #<li id="footballVideprinter_match_3253942_event_13267446">GOAL: <strong>Portugal 1-</strong>0 Mozambique : <strong>Danny</strong><strong class="minutesIntoMatch"> (52)</strong></li>
 
 class MatchIncident
-	attr_accessor :type, :minutes, :person, :country, :matchid
+    attr_accessor :type, :minutes, :person, :country, :matchid
 
-	def initialize(value)
-		# Set defaults
-		@type = IncidentType::UNKNOWN
-		@minutes = 0
-		@person = 'N/A'
-		@country = 'N/A'
-		@matchid = 0
+    def initialize(value)
+        # Set defaults
+        @type = IncidentType::UNKNOWN
+        @minutes = 0
+        @person = 'N/A'
+        @country = 'N/A'
+        @matchid = 0
 
-		@value = value
-		# Calculate the rest
-		determineType
-		determineMinutes
-		determineMatchID
-	end
+        @value = value
+        # Calculate the rest
+        determineType
+        determineMinutes
+        determineMatchID
+    end
 
-	# All methods below this point are private
-	private
+    # All methods below this point are private
+    private
 
-	# What was the type of incident
-	def determineType
-		html = @value.inner_html
-		id = @value.attributes['id']
+    # What was the type of incident
+    def determineType
+        html = @value.inner_html
+        id = @value.attributes['id']
 
-		if html.include? 'GOAL: '
-			@type = IncidentType::GOAL
-			countrySpecificIncidents
-		elsif id.include? '_HalfTime'
-			@type = IncidentType::HALF_TIME
-		elsif id.include? '_FullTime'
-			@type = IncidentType::FULL_TIME
+        if html.include? 'GOAL: '
+            @type = IncidentType::GOAL
+        countrySpecificIncidents
+        elsif id.include? '_HalfTime'
+            @type = IncidentType::HALF_TIME
+        elsif id.include? '_FullTime'
+            @type = IncidentType::FULL_TIME
         elsif id.include? '_Result'
             @type = IncidentType::RESULT
-		elsif html.include? 'SENT OFF:'
-			@type = IncidentType::SENT_OFF
-			countrySpecificIncidents
-		end
-	end
+        elsif html.include? 'SENT OFF:'
+            @type = IncidentType::SENT_OFF
+            countrySpecificIncidents
+        end
+    end
 
     # Some values only make sense for certain incidents
     # TODO: OOP - Make them a sub class
-	def countrySpecificIncidents
-    	determinePerson
-		determineCountry
+    def countrySpecificIncidents
+        determinePerson
+        determineCountry
     end
 
-	# What country scored/had a player sent off?
-	def determineCountry
-		unclean = @value.search("strong[1]").inner_html.strip
-		# Remove the current score - we can calculate this
-		# TODO: Store this for later for validation
-		@country = unclean.gsub(/\d+-/,'')
-	end
+    # What country scored/had a player sent off?
+    def determineCountry
+        unclean = @value.search("strong[1]").inner_html.strip
+        # Remove the current score - we can calculate this
+        # TODO: Store this for later for validation
+        @country = unclean.gsub(/\d+-/,'')
+    end
 
-	# When did the Incident happen?
-	def determineMinutes
-		if @type == IncidentType::HALF_TIME
-			@minutes = 45
-		elsif @type == IncidentType::FULL_TIME
-			@minutes = 90
-		elsif @type == IncidentType::GOAL
-			mins = @value.search("strong[@class='minutesIntoMatch']")
-			if mins.any?
-				@minutes = mins[0].inner_html.strip.scan(/\d+/)
-			end
-		end
-	end
+    # When did the Incident happen?
+    def determineMinutes
+        if @type == IncidentType::HALF_TIME
+            @minutes = 45
+        elsif @type == IncidentType::FULL_TIME
+            @minutes = 90
+        elsif @type == IncidentType::GOAL
+            mins = @value.search("strong[@class='minutesIntoMatch']")
+            if mins.any?
+                @minutes = mins[0].inner_html.strip.scan(/\d+/)
+            end
+        end
+    end
 
     # Who was involved in the incident?
-	def determinePerson
+    def determinePerson
         @person = @value.search("strong[2]").inner_html
-	end
+    end
 
-	# The BBC ID of the match
-	def determineMatchID
-		@matchid = @value.get_attribute('id').split('_')[2]
-	end
+    # The BBC ID of the match
+    def determineMatchID
+        @matchid = @value.get_attribute('id').split('_')[2]
+    end
 end
 
 class IncidentType
-	GOAL      = 'GOAL'
-	SENT_OFF  = 'SENT_OFF'
-	HALF_TIME = 'HALF_TIME'
-	FULL_TIME = 'FULL_TIME'
-	RESULT    = 'RESULT'
-	UNKNOWN   = 'UNKNOWN'
+    GOAL      = 'GOAL'
+    SENT_OFF  = 'SENT_OFF'
+    HALF_TIME = 'HALF_TIME'
+    FULL_TIME = 'FULL_TIME'
+    RESULT    = 'RESULT'
+    UNKNOWN   = 'UNKNOWN'
 end
 
